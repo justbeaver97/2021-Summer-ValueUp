a8c59cf7a7a09c2ddb5409e81ef16c113a66f8b7
justbeaver97 <73840274+justbeaver97@users.noreply.github.com>
2021-07-06 12:18:44 +0900

    Initial Commit

---
+++ iOS Application/iOS Application/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS Application
+//
+//  Created by 저스트비버 on 2021/07/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS Application/iOS Application/iOS_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_ApplicationApp.swift
+//  iOS Application
+//
+//  Created by 저스트비버 on 2021/07/06.
+//
+
+import SwiftUI
+
+@main
+struct iOS_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
