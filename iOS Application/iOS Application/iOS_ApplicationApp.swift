36616734e1b3591ced7409a7de45b92a2665928c
justbeaver97 <73840274+justbeaver97@users.noreply.github.com>
2021-07-07 17:15:54 +0900

    TODO: Place an Object

--- iOS Application/iOS Application/iOS_ApplicationApp.swift
+++ iOS Application/iOS Application/iOS_ApplicationApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct iOS_ApplicationApp: App {
+    @StateObject var placementSettings = PlacementSettings()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(placementSettings)
         }
     }
 }
