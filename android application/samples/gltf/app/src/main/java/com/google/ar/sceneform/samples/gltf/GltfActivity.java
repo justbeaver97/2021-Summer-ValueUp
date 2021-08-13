cb48f7137a1442cd143c30e10fded3ff6d9a9d50
Jungjjeong <72294509+Jungjjeong@users.noreply.github.com>
2021-08-13 15:16:16 +0900

    fix : clean code + lambda func

--- android application/samples/gltf/app/src/main/java/com/google/ar/sceneform/samples/gltf/GltfActivity.java
+++ android application/samples/gltf/app/src/main/java/com/google/ar/sceneform/samples/gltf/GltfActivity.java
@@ -17,8 +17,6 @@
  */
 package com.google.ar.sceneform.samples.gltf;
 
-import static java.util.concurrent.TimeUnit.SECONDS;
-
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Context;
@@ -28,8 +26,6 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.internal.NavigationMenu;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -37,13 +33,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.ArraySet;
 import android.util.Log;
 import android.view.Gravity;
-import android.view.MotionEvent;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
-import android.widget.Button;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.google.android.filament.gltfio.Animator;
 import com.google.android.filament.gltfio.FilamentAsset;
@@ -51,29 +45,19 @@ import com.google.ar.core.Anchor;
 import com.google.ar.core.Frame;
 import com.google.ar.core.HitResult;
 import com.google.ar.core.Plane;
-import com.google.ar.core.Session;
 import com.google.ar.core.Trackable;
 import com.google.ar.sceneform.AnchorNode;
-import com.google.ar.sceneform.ArSceneView;
-import com.google.ar.sceneform.FrameTime;
-import com.google.ar.sceneform.Node;
-import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.Color;
 import com.google.ar.sceneform.rendering.Material;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.rendering.Renderable;
-import com.google.ar.sceneform.rendering.ViewRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
-import com.google.ar.sceneform.ux.TranslationController;
 
 import java.lang.ref.WeakReference;
 import java.util.Arrays;
-import java.util.EnumSet;
 import java.util.List;
 import java.util.Set;
-import java.util.logging.Logger;
-import java.util.Date;
 
 public class GltfActivity extends AppCompatActivity {
     private static final String TAG = GltfActivity.class.getSimpleName(); // log 띄우기 위해
@@ -131,7 +115,7 @@ public class GltfActivity extends AppCompatActivity {
         } // 지원하는 OpenGL 버전(3.0)이 적합한지, android sdk 버전이 맞는지 확인
 
         setContentView(R.layout.activity_ux);
-        ProgressBar progress = (ProgressBar) findViewById(R.id.progress);
+        ProgressBar progress = findViewById(R.id.progress);
         progress.setVisibility(View.GONE);
 
         final String[] Glburi = new String[22];
@@ -166,122 +150,109 @@ public class GltfActivity extends AppCompatActivity {
 
         int length = (int) intent.getSerializableExtra("size"); // get items -> key, size(length)
 
-        Button button_distance = (Button)findViewById(R.id.button_distance);
-        button_distance.setOnClickListener(new View.OnClickListener(){ // button Click -> setOnClickListener
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "거리 측정페이지입니다.", Toast.LENGTH_LONG).show();
-                Intent pageIntent = new Intent(GltfActivity.this, DistanceActivity.class); // 거리측정페이지 : DistanceActivity
-                pageIntent.putExtra("length", length); // length 전달
-                startActivity(pageIntent);
-            }
-
+        Button button_distance = findViewById(R.id.button_distance);
+        // button Click -> setOnClickListener
+        button_distance.setOnClickListener(v -> {
+            Toast.makeText(getApplicationContext(), "거리 측정페이지입니다.", Toast.LENGTH_LONG).show();
+            Intent pageIntent = new Intent(GltfActivity.this, DistanceActivity.class); // 거리측정페이지 : DistanceActivity
+            pageIntent.putExtra("length", length); // length 전달
+            startActivity(pageIntent);
         });
-        DrawerLayout drawerLayout = (DrawerLayout)findViewById(R.id.drawerLayout);
-        Button button_list = (Button)findViewById(R.id.button_list);
-        button_list.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){ drawerLayout.openDrawer(GravityCompat.START); }
-        });
+        DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+        Button button_list = findViewById(R.id.button_list);
+        button_list.setOnClickListener(v -> drawerLayout.openDrawer(GravityCompat.START));
 
 
         WeakReference<GltfActivity> weakActivity = new WeakReference<>(this); // Weakreference -> MemoryLeak X
         // 다른 Class에서 activity를 포함한 객체를 참조하거나, 별도의 스레드에서 view, activity를 참조하고 있는 경우, 해당 참조를 주어 메모리 누수를 방지한다.
         // sceneform의 ModelAnimator은 약한 참조만을 이용한다. 일반 soft나 strongreference를 사용하기 위해서는 해당 객체를 Node에 추가해야 한다.
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav);
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override // List에서 상품 선택시
-            public boolean onNavigationItemSelected(MenuItem menuItem) {
+        NavigationView navigationView = findViewById(R.id.nav);
+        // List에서 상품 선택시
+        navigationView.setNavigationItemSelectedListener(menuItem -> {
 
-                menuItem.setChecked(true);
-                drawerLayout.closeDrawers();
+            menuItem.setChecked(true);
+            drawerLayout.closeDrawers();
 
-                int id = menuItem.getItemId();
-                String title = menuItem.getTitle().toString();
+            int id = menuItem.getItemId();
+            String title = menuItem.getTitle().toString();
 
-                if(id==R.id.menu_668317){
-                    fileUri = Glburi[0];
-                    buildModel(weakActivity,getBaseContext(), fileUri, progress);
-                }else if(id == R.id.menu_668318){
-                    fileUri = Glburi[1];
-                    buildModel(weakActivity,getBaseContext(), fileUri, progress);
-                }else if(id == R.id.menu_681946){
-                    fileUri = Glburi[2];
-                    buildModel(weakActivity,getBaseContext(), fileUri, progress);
-                }else if(id == R.id.menu_681947){
-                    fileUri = Glburi[3];
-                    buildModel(weakActivity,getBaseContext(), fileUri, progress);
-                }else if(id == R.id.menu_737686){
-                    fileUri = Glburi[4];
-                    buildModel(weakActivity,getBaseContext(), fileUri, progress);
-                }else if(id == R.id.menu_737687){
-                    fileUri = Glburi[5];
-                    buildModel(weakActivity,getBaseContext(), fileUri, progress);
-                }else if(id == R.id.menu_746525){
-                    fileUri = Glburi[6];
-                    buildModel(weakActivity,getBaseContext(), fileUri, progress);
-                }else if(id == R.id.menu_746526){
-                    fileUri = Glburi[7];
-                    buildModel(weakActivity,getBaseContext(), fileUri, progress);
-                }else if(id == R.id.menu_746540){
-                    fileUri = Glburi[8];
-                    buildModel(weakActivity,getBaseContext(), fileUri, progress);
-                }else if(id == R.id.menu_746541){
-                    fileUri = Glburi[9];
-                    buildModel(weakActivity,getBaseContext(), fileUri, progress);
-                }else if(id == R.id.menu_772973){
-                    fileUri = Glburi[10];
-                    buildModel(weakActivity,getBaseContext(), fileUri, progress);
-                }else if(id == R.id.menu_777039){
-                    fileUri = Glburi[11];
-                    buildModel(weakActivity,getBaseContext(), fileUri, progress);
-                }else if(id == R.id.menu_777040){
-                    fileUri = Glburi[12];
-                    buildModel(weakActivity,getBaseContext(), fileUri, progress);
-                }else if(id == R.id.menu_786840){
-                    fileUri = Glburi[13];
-                    buildModel(weakActivity,getBaseContext(), fileUri, progress);
-                }else if(id == R.id.menu_786841){
-                    fileUri = Glburi[14];
-                    buildModel(weakActivity,getBaseContext(), fileUri, progress);
-                }else if(id == R.id.menu_786842){
-                    fileUri = Glburi[15];
-                    buildModel(weakActivity,getBaseContext(), fileUri, progress);
-                }else if(id == R.id.menu_787819){
-                    fileUri = Glburi[16];
-                    buildModel(weakActivity,getBaseContext(), fileUri, progress);
-                }else if(id == R.id.menu_787823){
-                    fileUri = Glburi[17];
-                    buildModel(weakActivity,getBaseContext(), fileUri, progress);
-                }else if(id == R.id.menu_796379){
-                    fileUri = Glburi[18];
-                    buildModel(weakActivity,getBaseContext(), fileUri, progress);
-                }else if(id == R.id.menu_796416){
-                    fileUri = Glburi[19];
-                    buildModel(weakActivity,getBaseContext(), fileUri, progress);
-                }else if(id == R.id.menu_799215){
-                    fileUri = Glburi[20];
-                    buildModel(weakActivity,getBaseContext(), fileUri, progress);
-                }else if(id == R.id.menu_799220){
-                    fileUri = Glburi[21];
-                    buildModel(weakActivity,getBaseContext(), fileUri, progress);
-                }
-
-                Toast.makeText(getApplicationContext(), "현재 상품 : " + title, Toast.LENGTH_LONG).show();
-
-                return true;
+            if(id==R.id.menu_668317){
+                fileUri = Glburi[0];
+                buildModel(weakActivity,getBaseContext(), fileUri, progress);
+            }else if(id == R.id.menu_668318){
+                fileUri = Glburi[1];
+                buildModel(weakActivity,getBaseContext(), fileUri, progress);
+            }else if(id == R.id.menu_681946){
+                fileUri = Glburi[2];
+                buildModel(weakActivity,getBaseContext(), fileUri, progress);
+            }else if(id == R.id.menu_681947){
+                fileUri = Glburi[3];
+                buildModel(weakActivity,getBaseContext(), fileUri, progress);
+            }else if(id == R.id.menu_737686){
+                fileUri = Glburi[4];
+                buildModel(weakActivity,getBaseContext(), fileUri, progress);
+            }else if(id == R.id.menu_737687){
+                fileUri = Glburi[5];
+                buildModel(weakActivity,getBaseContext(), fileUri, progress);
+            }else if(id == R.id.menu_746525){
+                fileUri = Glburi[6];
+                buildModel(weakActivity,getBaseContext(), fileUri, progress);
+            }else if(id == R.id.menu_746526){
+                fileUri = Glburi[7];
+                buildModel(weakActivity,getBaseContext(), fileUri, progress);
+            }else if(id == R.id.menu_746540){
+                fileUri = Glburi[8];
+                buildModel(weakActivity,getBaseContext(), fileUri, progress);
+            }else if(id == R.id.menu_746541){
+                fileUri = Glburi[9];
+                buildModel(weakActivity,getBaseContext(), fileUri, progress);
+            }else if(id == R.id.menu_772973){
+                fileUri = Glburi[10];
+                buildModel(weakActivity,getBaseContext(), fileUri, progress);
+            }else if(id == R.id.menu_777039){
+                fileUri = Glburi[11];
+                buildModel(weakActivity,getBaseContext(), fileUri, progress);
+            }else if(id == R.id.menu_777040){
+                fileUri = Glburi[12];
+                buildModel(weakActivity,getBaseContext(), fileUri, progress);
+            }else if(id == R.id.menu_786840){
+                fileUri = Glburi[13];
+                buildModel(weakActivity,getBaseContext(), fileUri, progress);
+            }else if(id == R.id.menu_786841){
+                fileUri = Glburi[14];
+                buildModel(weakActivity,getBaseContext(), fileUri, progress);
+            }else if(id == R.id.menu_786842){
+                fileUri = Glburi[15];
+                buildModel(weakActivity,getBaseContext(), fileUri, progress);
+            }else if(id == R.id.menu_787819){
+                fileUri = Glburi[16];
+                buildModel(weakActivity,getBaseContext(), fileUri, progress);
+            }else if(id == R.id.menu_787823){
+                fileUri = Glburi[17];
+                buildModel(weakActivity,getBaseContext(), fileUri, progress);
+            }else if(id == R.id.menu_796379){
+                fileUri = Glburi[18];
+                buildModel(weakActivity,getBaseContext(), fileUri, progress);
+            }else if(id == R.id.menu_796416){
+                fileUri = Glburi[19];
+                buildModel(weakActivity,getBaseContext(), fileUri, progress);
+            }else if(id == R.id.menu_799215){
+                fileUri = Glburi[20];
+                buildModel(weakActivity,getBaseContext(), fileUri, progress);
+            }else if(id == R.id.menu_799220){
+                fileUri = Glburi[21];
+                buildModel(weakActivity,getBaseContext(), fileUri, progress);
             }
 
+            Toast.makeText(getApplicationContext(), "현재 상품 : " + title, Toast.LENGTH_LONG).show();
+
+            return true;
         });
 
-        Button button_refresh = (Button)findViewById(R.id.button_refresh);
-        button_refresh.setOnClickListener(new View.OnClickListener(){
-            @Override // anchor 초기화
-            public void onClick(View v){
-                animators.clear();
-            }
-        });
+        Button button_refresh = findViewById(R.id.button_refresh);
+        // anchor 초기화
+        button_refresh.setOnClickListener(v -> animators.clear());
 
         // -----
 
@@ -344,55 +315,54 @@ public class GltfActivity extends AppCompatActivity {
 //                        }
 //        );
 
-        Button addButton = (Button) findViewById(R.id.button_add);
-        addButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                if (renderable == null) {
-                    return;
-                }
-                HitResult hitResult = null;
-                android.graphics.Point pt = new Point(arFragment.getArSceneView().getWidth() / 2, arFragment.getArSceneView().getHeight() /2);
-                List<HitResult> hits;
-                Frame frame = arFragment.getArSceneView().getArFrame();
-                if (frame != null) {
-                    hits = frame.hitTest(pt.x, pt.y);
-                    for (HitResult hit : hits) {
-                        Trackable trackable = hit.getTrackable();
-                        if (trackable instanceof Plane && ((Plane) trackable).isPoseInPolygon(hit.getHitPose())) {
-                            hitResult = hit;
-                            break;
-                        }
+        Button addButton = findViewById(R.id.button_add);
+        addButton.setOnClickListener(v -> {
+            if (renderable == null) {
+                return;
+            }
+            HitResult hitResult = null;
+            Point pt = new Point(arFragment.getArSceneView().getWidth() / 2, arFragment.getArSceneView().getHeight() /2);
+            List<HitResult> hits;
+            Frame frame = arFragment.getArSceneView().getArFrame();
+            if (frame != null) {
+                hits = frame.hitTest(pt.x, pt.y);
+                for (HitResult hit : hits) {
+                    Trackable trackable = hit.getTrackable();
+                    if (trackable instanceof Plane && ((Plane) trackable).isPoseInPolygon(hit.getHitPose())) {
+                        hitResult = hit;
+                        break;
                     }
                 }
-                // Create the Anchor.
-                Anchor anchor = hitResult.createAnchor(); // create anchor
-                AnchorNode anchorNode = new AnchorNode(anchor); // Object가 배치되는 영역인 Node를 Anchor 할당 생성 -> AnchorNode
-                anchorNode.setParent(arFragment.getArSceneView().getScene()); // (getScene : 장면 반환 / getArSceneView : 장면 랜더링(arsceneview) 반환) -> parentNode로 set
+            }
+            // Create the Anchor.
+            assert hitResult != null;
+            Anchor anchor = hitResult.createAnchor(); // create anchor
+            AnchorNode anchorNode = new AnchorNode(anchor); // Object가 배치되는 영역인 Node를 Anchor 할당 생성 -> AnchorNode
+            anchorNode.setParent(arFragment.getArSceneView().getScene()); // (getScene : 장면 반환 / getArSceneView : 장면 랜더링(arsceneview) 반환) -> parentNode로 set
 
-                // Create the transformable model and add it to the anchor.
-                TransformableNode model = new TransformableNode(arFragment.getTransformationSystem()); // TransformableNode -> 선택, 변환, 회전, 크기 조정 가능한 Node
-                model.setRenderable(renderable); // set rendering model
-                model.getScaleController().setMaxScale(0.015f);
-                model.getScaleController().setMinScale(0.005f); // set Scale
-                model.setParent(anchorNode); // Anchor node 위에 model set -> 부모 설정
+            // Create the transformable model and add it to the anchor.
+            TransformableNode model = new TransformableNode(arFragment.getTransformationSystem()); // TransformableNode -> 선택, 변환, 회전, 크기 조정 가능한 Node
+            model.setRenderable(renderable); // set rendering model
+            model.getScaleController().setMaxScale(0.015f);
+            model.getScaleController().setMinScale(0.005f); // set Scale
+            model.setParent(anchorNode); // Anchor node 위에 model set -> 부모 설정
 
-                model.select();
+            model.select();
 
-                // Filament -> android, iOS 등 WebGL을 위한 실시간 Rendering engine
-                FilamentAsset filamentAsset = model.getRenderableInstance().getFilamentAsset(); // filamentAsset = filament에서 사용할 3D 모델(.glb file) 정의
-                if (filamentAsset.getAnimator().getAnimationCount() > 0) {
-                    animators.add(new AnimationInstance(filamentAsset.getAnimator(), 0, System.nanoTime())); // Array set animators -> add Instance
-                }
-
-                Color color = colors.get(nextColor); // basic color setting
-                nextColor++;
-                for (int i = 0; i < renderable.getSubmeshCount(); ++i) {
-                    Material material = renderable.getMaterial(i);
-                    material.setFloat4("baseColorFactor", color);
-                }
+            // Filament -> android, iOS 등 WebGL을 위한 실시간 Rendering engine
+            assert model.getRenderableInstance() != null;
+            FilamentAsset filamentAsset = model.getRenderableInstance().getFilamentAsset(); // filamentAsset = filament에서 사용할 3D 모델(.glb file) 정의
+            assert filamentAsset != null;
+            if (filamentAsset.getAnimator().getAnimationCount() > 0) {
+                animators.add(new AnimationInstance(filamentAsset.getAnimator(), 0, System.nanoTime())); // Array set animators -> add Instance
             }
 
+            Color color = colors.get(nextColor); // basic color setting
+            nextColor++;
+            for (int i = 0; i < renderable.getSubmeshCount(); ++i) {
+                Material material = renderable.getMaterial(i);
+                material.setFloat4("baseColorFactor", color);
+            }
         });
 
     }
